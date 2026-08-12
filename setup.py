c387cfe059e9173e7ead3f92b576c5f7f3cb03e1
“MFerdus” <erdusmstzannatun1@gmail.com>
2026-08-12 00:10:15 -0400

    comitted files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Zannat",
+    author_email="mst.ferdus@student.potomac.edu",
+    packages=find_packages()
+)
